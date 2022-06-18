de63846fbdcdcf1ce20d2994666ea495332c62e6
Ajulll22 <ajulrizki@gmail.com>
2022-06-18 20:42:08 +0700

    main app

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"siakad-rest/config"
+	"siakad-rest/db"
+	"siakad-rest/pkg/auth"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	app := fiber.New()
+	db := db.Init(c.DBUrl)
+
+	auth.RegisterRoutes(app, db)
+
+	// books.RegisterRoutes(app, db)
+
+	app.Listen(c.Port)
+}
