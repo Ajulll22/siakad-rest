68c5cd9a912a5dd146941d7366f49f3306bc3794
Ajulll22 <ajulrizki@gmail.com>
2022-06-26 21:42:16 +0700

    hendler logout

---
+++ pkg/auth/logout.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func (h handler) Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "Success",
+	})
+}
