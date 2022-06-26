babd36fdd2ccafadd85be21a45590d8779a05e90
Ajulll22 <ajulrizki@gmail.com>
2022-06-26 21:41:40 +0700

    controller auth

---
+++ pkg/auth/controller.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type handler struct {
+	DB *gorm.DB
+}
+
+func RegisterRoutes(app *fiber.App, db *gorm.DB) {
+	h := &handler{
+		DB: db,
+	}
+
+	routes := app.Group("/api/auth")
+	routes.Post("/login", h.Login)
+	routes.Post("/logout", h.Logout)
+
+}
