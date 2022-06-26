aa1f4d9daaf83c560a17df940e925d0b14000e87
Ajulll22 <ajulrizki@gmail.com>
2022-06-26 21:42:47 +0700

    controller user

---
+++ pkg/user/controller.go
@@ -0,0 +1,20 @@
+package user
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type handler struct {
+	DB *gorm.DB
+}
+
+func RegisterRoutes(app *fiber.App, db *gorm.DB) {
+	h := &handler{
+		DB: db,
+	}
+
+	routes := app.Group("/api/user")
+	routes.Get("/", h.GetUsers)
+
+}
